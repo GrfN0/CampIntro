a1a66e7883ecbc40ac5eb0c142dd043040acdac5
samet yılmaz <samet_yilmaz07@hotmail.com>
2021-01-23 12:42:43 +0300

    Ödev 5

---
+++ DictionaryIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(10, "Samet");
+            myDictionary.Add(9, "Murat");
+            myDictionary.Add(8, "Gökhan");
+            myDictionary.Add(7, "Çağrı");
+
+            foreach (var names in myDictionary.Keys)
+            {
+                Console.WriteLine(names);
+                
+            }   
+
+            foreach (var numbers in myDictionary.Values)
+            {
+                Console.WriteLine(numbers);
+            }
+            
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety 
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalis butonu");
+            }
+            else if (dolarDun < dolarBugun) ;
+            {
+                Console.WriteLine("Artis oku");
+
+            }
+            else
+            {
+                Console.WriteLine("Degismedi oku");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
